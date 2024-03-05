f3b37c344158c19a1453f693968ad71d4beb9c0e
Kurenairisu <minec323@gmail.com>
2024-03-05 23:59:37 +0800

    i hate ril

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 
-use std::path::Path;
+use std::{collections::HashMap, path::Path};
 
 use actix_web::{get, post, App, HttpResponse, HttpServer, Responder};
-use ril::{Image, Rgb, TextAlign, Font, TextLayout, WrapStyle, TextSegment, Rgba};
+use ril::{Font, GradientBlendMode, Image, PalettedRgba, ResizeAlgorithm, Rgb, Rgba, TextAlign, TextLayout, TextSegment, WrapStyle};
 
 #[get("/ping")]
 async fn hello() -> impl Responder {
@@ -43,8 +43,12 @@ fn create_image() -> ril::Result<Vec<u8>> {
 
     let mut image = Image::new(1280, 720, Rgba::new(255, 255, 255, 0));
     let gradient = Image::<Rgba>::open(Path::new("./media/images/overlay_gradient.png"))?;
-    let pfp = Image::<Rgba>::open(Path::new("./media/images/profile.png"))?;
-
+    let mut pfp = Image::<Rgba>::open(Path::new("./media/images/profile.png"))?;
+    let aspect_ratio = pfp.width() as f32 / pfp.height() as f32;
+    let new_height = image.height();
+    let new_width = (new_height as f32 * aspect_ratio).round() as u32;
+    pfp.resize(new_width, new_height, ResizeAlgorithm::Nearest);
+    
     let font = Font::open("./media/fonts/arial.ttf", 36.0)?;
     let (x, y) = image.center();
     let layout = TextLayout::<Rgba>::new()
@@ -57,11 +61,132 @@ fn create_image() -> ril::Result<Vec<u8>> {
             &TextSegment::<Rgba>::new(&font, "i love kogasa i love youmu i love kogasa i love kogasa i love youmu i love kogasa i love kogasa i love youmu i love kogasa ", Rgba::white())
             .with_size(36.0)
         );
+
     image.paste(0, 0, &pfp);
-    image.paste(0, 0, &gradient);
+
+    for y in 0..gradient.height() {
+        for x in 0..gradient.width() {
+            let dest_pixel = image.pixel_mut(x, y);
+            let src_pixel = gradient.pixel(x, y);
+            let alpha = src_pixel.a as f32 / 255.0;
+            let inv_alpha = 1.0 - alpha;
+            dest_pixel.r = (dest_pixel.r as f32 * inv_alpha + src_pixel.r as f32 * alpha) as u8;
+            dest_pixel.g = (dest_pixel.g as f32 * inv_alpha + src_pixel.g as f32 * alpha) as u8;
+            dest_pixel.b = (dest_pixel.b as f32 * inv_alpha + src_pixel.b as f32 * alpha) as u8;
+            dest_pixel.a = (dest_pixel.a as f32 * inv_alpha + src_pixel.a as f32 * alpha) as u8;
+        }
+    }
+
     image.draw(&layout);
 
     image.save_inferred("bruhhh.png")?;
-    // image.encode(ImageFormat::Png, &mut bytes).unwrap();
     Ok(bytes)
+}
+
+fn load_piece_board_images() -> ril::Result<(HashMap<char, PalettedRgba<'static>>, HashMap<char, PalettedRgba<'static>>)> {
+    let piece_images: HashMap<char, PalettedRgba> = [
+        // Black Pieces
+        (
+            'p',
+            Image::<Rgba>::open(Path::new("./media/images/overlay_gradient.png"))?,
+
+        ),
+        (
+            'r',
+            Image::<Rgba>::open(Path::new("./media/images/overlay_gradient.png"))?,
+
+        ),
+        (
+            'n',
+            Image::<Rgba>::open(Path::new("./media/images/overlay_gradient.png"))?,
+
+        ),
+        (
+            'b',
+            Image::<Rgba>::open(Path::new("./media/images/overlay_gradient.png"))?,
+
+        ),
+        (
+            'q',
+            Image::<Rgba>::open(Path::new("./media/images/overlay_gradient.png"))?,
+        ),
+        (
+            'k',
+            Image::<Rgba>::open(Path::new("./media/images/overlay_gradient.png"))?,
+
+        ),
+        // White Pieces
+        (
+            'P',
+            Image::<Rgba>::open(Path::new("./media/images/overlay_gradient.png"))?,
+        ),
+        (
+            'R',
+            Image::<Rgba>::open(Path::new("./media/images/overlay_gradient.png"))?,
+        ),
+        (
+            'N',
+            Image::<Rgba>::open(Path::new("./media/images/overlay_gradient.png"))?,
+        ),
+        (
+            'B',
+            Image::<Rgba>::open(Path::new("./media/images/overlay_gradient.png"))?,
+        ),
+        (
+            'Q',
+            Image::<Rgba>::open(Path::new("./media/images/overlay_gradient.png"))?,
+        ),
+        (
+            'K',
+            Image::<Rgba>::open(Path::new("./media/images/overlay_gradient.png"))?,
+        ),
+    ]
+    .iter()
+    .cloned()
+    .collect();
+
+    let board_image =             Image::<Rgba>::open(Path::new("./media/images/overlay_gradient.png"))?;
+}
+
+
+
+
+
+fn fen_to_board_img(fen: &str, save_dir: &str, upscale_multiplier: u32, piece_images: &HashMap<char, RgbaImage>, board_image: &RgbaImage) {
+    let board = fen.split_whitespace().next().unwrap();
+    let mut img = board_image.clone();
+    let square_size = (img.width() - 8) / 8; 
+    let piece_size = 16;
+    let offset = (square_size - piece_size) / 2;
+    let border_size = 4;
+    let mut x = 0;
+    let mut y = 0;
+    for char in board.chars() {
+        if char == '/' {
+            y += 1;
+            x = 0;
+            continue;
+        }
+        if let Some(digit) = char.to_digit(10) {
+            x += digit;
+            continue;
+        }
+        if let Some(piece_image) = piece_images.get(&char) {
+            overlay(
+                &mut img,
+                piece_image,
+                (x * square_size + offset + border_size) as i64,
+                (y * square_size + offset + border_size) as i64,
+            );
+        }
+        x += 1;
+    }
+
+    let new_width = img.width() * upscale_multiplier;
+    let new_height = img.height() * upscale_multiplier;
+    let upscale_filter = image::imageops::FilterType::Nearest;
+
+    let upscaled_img = image::imageops::resize(&img, new_width, new_height, upscale_filter);
+
+    upscaled_img.save(save_dir).unwrap();
 }
