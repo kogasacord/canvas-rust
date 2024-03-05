7043b859c98760f69dfadf62c4fea68e63595d1a
DoormatIka <trashcanartistlily@gmail.com>
2024-03-05 17:51:25 +0800

    fully functioning quote + font resizing

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 
-use std::path::Path;
+use std::{path::Path, cmp};
 
 use actix_web::{get, post, App, HttpResponse, HttpServer, Responder};
-use ril::{Image, Rgb, TextAlign, Font, TextLayout, WrapStyle, TextSegment, Rgba};
+use ril::{Image, Paste, TextAlign, Font, TextLayout, WrapStyle, TextSegment, Rgba, ResizeAlgorithm, OverlayMode, Rgb};
 
 #[get("/ping")]
 async fn hello() -> impl Responder {
@@ -17,7 +17,8 @@ async fn echo(req_body: String) -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    match create_image() {
+    let mut quote = Quote::new("./media/images/overlay_gradient.png", "./media/images/profile.png").unwrap();
+    match create_quote(&mut quote, "hiiii", "alice") {
         Ok(_) => {
             Ok(())
             /*
@@ -38,30 +39,85 @@ async fn main() -> std::io::Result<()> {
     
 }
 
-fn create_image() -> ril::Result<Vec<u8>> {
+/**
+ * https://www.desmos.com/calculator/4t4e16y73m
+ */
+fn font_size_estimate(text: &str) -> f32 {
+    let constrained_len = cmp::max(cmp::min(text.len(), 130), 1);
+    -(constrained_len as f32 * 0.245) + 60.0
+}
+
+struct Quote {
+    image: Image<Rgba>,
+    gradient: Image<Rgba>,
+    pfp: Image<Rgba>,
+    font: Font,
+}
+impl Quote {
+    fn new(gradient_path: &str, pfp_path: &str) -> ril::Result<Quote> {
+        let image = Image::new(1280, 720, Rgba::new(255, 255, 255, 0));
+        let gradient = Image::<Rgba>::open(Path::new(gradient_path))?;
+        // needs to be changed in order to accept pfps from buffers/Vec<u8>s
+        let mut pfp = Image::<Rgba>::open(Path::new(pfp_path))?;
+        let font = Font::open("./media/fonts/playfair.ttf", 48.0)?;
+
+        let aspect_ratio = pfp.width() as f32 / pfp.height() as f32;
+        let new_height = image.height();
+        let new_width = (new_height as f32 * aspect_ratio).round() as u32;
+        pfp.resize(new_width, new_height, ResizeAlgorithm::Lanczos3);
+
+        Ok(Self { 
+            image, 
+            gradient, 
+            pfp, 
+            font 
+        })
+    }
+    fn combine_pfp_and_gradient(&mut self) {
+        self.image.draw(&Paste::new(&self.pfp)
+                   .with_overlay_mode(OverlayMode::Merge)
+                   .with_position(0, 0));
+        self.image.draw(&Paste::new(&self.gradient)
+                   .with_overlay_mode(OverlayMode::Merge));
+    }
+    fn get_underlying_image(&mut self) -> &mut Image<Rgba> {
+        &mut self.image
+    }
+}
+
+fn create_quote(quote: &mut Quote, text: &str, author: &str) -> ril::Result<Vec<u8>> {
     let mut bytes = Vec::new();
+    let estimated_font_size = font_size_estimate(text);
 
-    let mut image = Image::new(1280, 720, Rgba::new(255, 255, 255, 0));
-    let gradient = Image::<Rgba>::open(Path::new("./media/images/overlay_gradient.png"))?;
-    let pfp = Image::<Rgba>::open(Path::new("./media/images/profile.png"))?;
-
-    let font = Font::open("./media/fonts/arial.ttf", 36.0)?;
-    let (x, y) = image.center();
-    let layout = TextLayout::<Rgba>::new()
+    let font = Font::open("./media/fonts/playfair.ttf", 48.0)?;
+    let (x, y) = quote.get_underlying_image().center();
+    let text_layout = TextLayout::<Rgba>::new()
         .centered()
         .with_wrap(WrapStyle::Word)
-        .with_width(image.width() / 3)
+        .with_width(quote.get_underlying_image().width() / 3)
         .with_align(TextAlign::Center)
         .with_position(x + 350, y)
         .with_segment(
-            &TextSegment::<Rgba>::new(&font, "i love kogasa i love youmu i love kogasa i love kogasa i love youmu i love kogasa i love kogasa i love youmu i love kogasa ", Rgba::white())
-            .with_size(36.0)
-        );
-    image.paste(0, 0, &pfp);
-    image.paste(0, 0, &gradient);
-    image.draw(&layout);
+            &TextSegment::<Rgba>::new(&font, text, Rgba::white())
+                .with_size(estimated_font_size));
 
-    image.save_inferred("bruhhh.png")?;
+    let (_x, _y, _max_x, max_y) = text_layout.bounding_box();
+    let author_layout = TextLayout::<Rgba>::new()
+        .with_horizontal_anchor(ril::HorizontalAnchor::Left)
+        .with_wrap(WrapStyle::Word)
+        .with_width(quote.get_underlying_image().width() / 5)
+        .with_align(TextAlign::Right)
+        .with_position(x + 400, max_y)
+        .with_segment(
+            &TextSegment::<Rgba>::new(&font, format!("\n - {}", author), Rgba::white())
+                .with_size(24.0));
+
+    quote.combine_pfp_and_gradient();
+
+    quote.get_underlying_image().draw(&text_layout);
+    quote.get_underlying_image().draw(&author_layout);
+
+    quote.get_underlying_image().save_inferred("bruhhh.png")?;
     // image.encode(ImageFormat::Png, &mut bytes).unwrap();
     Ok(bytes)
 }
